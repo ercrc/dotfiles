64ffd2bd5cc081907efd09e49581d243ad8e64f5
ercrc <eriq2077@gmail.com>
2025-07-14 23:54:43 -0300

    refactor

--- .config/wezterm/wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-
-config.color_scheme = 'Gruvbox Material (Gogh)'
-config.enable_tab_bar = false
-config.window_background_opacity = 0.94
-
-return config 
